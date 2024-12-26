11c49f90cec2ac8e2ed882348a5cc0b92a80e67b
Tanuja0517-bts-monstax <tanujans0803@gmail.com>
2024-12-26 12:57:49 +0530

    updates class name

--- SimpleMavenProject/src/test/java/tesrScript/Vtiger_Test.java
+++ SimpleMavenProject/src/test/java/tesrScript/Vtiger_Test.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Vtiger {
+public class Vtiger_Test {
 	@Test
 	public void loginToApp_Test() {
 		WebDriver driver = new ChromeDriver();
